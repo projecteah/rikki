15c12ef5180d9f0d7930bc2df47a595dd43c56a3
Xun Lee <75558996+IlIllIIlIl@users.noreply.github.com>
2022-10-13 16:32:07 +0800

    feat: add markdown notes with vue3 + element-plus

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 700,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
-  win.loadFile('index.html');
+  win.loadFile('notes.html');
 }
 
 app.whenReady().then(createWindow);
