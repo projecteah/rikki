c27ab74cbe0d2c6873fdc3a05e620f544cda460e
Xun Lee <75558996+IlIllIIlIl@users.noreply.github.com>
2022-11-23 14:28:15 +0800

    feat: quick input and timeline feed

--- apps/web/composables/useEditor.ts
+++ apps/web/composables/useEditor.ts
@@ -1,27 +1,22 @@
 import { marked } from 'marked'
 
-export function useEditor(current: ReturnType<typeof useNotes>['current']) {
-  const editor = ref<any>(null)
-
-  const format = (before: string, after?: string) => {
-    const el = editor.value?.$refs?.textarea as HTMLTextAreaElement
-    if (!el) return
-
-    const start = el.selectionStart
-    const end = el.selectionEnd
-    const text = current.value.content || ''
+export function useEditor() {
+  const format = (textarea: HTMLTextAreaElement, before: string, after?: string) => {
+    const start = textarea.selectionStart
+    const end = textarea.selectionEnd
+    const text = textarea.value
     const selected = text.substring(start, end)
     const replacement = before + (selected || 'text') + (after || '')
 
-    current.value.content = text.substring(0, start) + replacement + text.substring(end)
+    textarea.value = text.substring(0, start) + replacement + text.substring(end)
 
     setTimeout(() => {
-      el.focus()
-      el.setSelectionRange(start + before.length, start + before.length + (selected ? selected.length : 4))
+      textarea.focus()
+      textarea.setSelectionRange(start + before.length, start + before.length + (selected ? selected.length : 4))
     })
   }
 
-  const rendered = computed(() => marked.parse(current.value.content || ''))
+  const render = (content: string) => marked.parse(content)
 
-  return { editor, format, rendered }
+  return { format, render }
 }
--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -1,53 +1,71 @@
 <script setup lang="ts">
-const { notes, activeIndex, current, newNote, selectNote, deleteNote } = useNotes()
-const { editor, format, rendered } = useEditor(current)
+const { memos, addMemo, deleteMemo, allTags } = useMemos()
+const { format, render } = useEditor()
 const { isDark, toggleDark } = useTheme()
 
-const contextIndex = ref(0)
+const input = ref('')
+const textarea = ref<HTMLTextAreaElement | null>(null)
 
-function onContextCommand(cmd: string, i: number) {
-  if (cmd === 'delete') deleteNote(i)
+const submit = () => {
+  const text = input.value.trim()
+  if (!text) return
+  addMemo(text)
+  input.value = ''
+}
+
+const onFormat = (before: string, after?: string) => {
+  if (textarea.value) format(textarea.value, before, after)
 }
 </script>
 
 <template>
-  <el-container>
-    <el-aside width="220px" class="flex flex-col">
-      <div class="flex items-center justify-between p-2">
-        <el-button type="primary" size="small" @click="newNote">+ New</el-button>
-        <el-switch v-model="isDark" @change="toggleDark" />
+  <div class="flex flex-col h-screen">
+    <header class="flex items-center justify-between px-4 py-3 border-b border-[var(--el-border-color)]">
+      <h1 class="text-lg font-semibold">Rikki</h1>
+      <el-switch v-model="isDark" @change="toggleDark" />
+    </header>
+
+    <div class="px-4 py-3 border-b border-[var(--el-border-color)]">
+      <textarea
+        ref="textarea"
+        v-model="input"
+        placeholder="what's on your mind..."
+        class="w-full resize-none border-none outline-none text-sm bg-transparent"
+        rows="3"
+        @keydown.meta.enter="submit"
+        @keydown.ctrl.enter="submit"
+      />
+      <div class="flex items-center justify-between mt-2">
+        <div class="flex gap-1 flex-wrap">
+          <el-button size="small" @click="onFormat('**', '**')"><b>B</b></el-button>
+          <el-button size="small" @click="onFormat('*', '*')"><i>I</i></el-button>
+          <el-button size="small" @click="onFormat('# ')">H1</el-button>
+          <el-button size="small" @click="onFormat('## ')">H2</el-button>
+          <el-button size="small" @click="onFormat('- ')">List</el-button>
+          <el-button size="small" @click="onFormat('[](url)')">Link</el-button>
+          <el-button size="small" @click="onFormat('`', '`')">Code</el-button>
+          <el-button size="small" @click="onFormat('> ')">Quote</el-button>
+        </div>
+        <el-button type="primary" size="small" @click="submit">send</el-button>
       </div>
-      <el-menu :default-active="activeIndex" @select="selectNote">
-        <el-menu-item v-for="(note, i) in notes" :key="i" :index="String(i)">
-          <span class="truncate flex-1">{{ note.title || 'Untitled' }}</span>
-          <el-dropdown trigger="click" @command="onContextCommand($event, i)" class="ml-auto cursor-pointer p-1 hover:bg-black/5 rounded">
-            <span class="text-base leading-none">⋮</span>
-            <template #dropdown>
-              <el-dropdown-menu>
-                <el-dropdown-item command="delete">Delete</el-dropdown-item>
-              </el-dropdown-menu>
-            </template>
-          </el-dropdown>
-        </el-menu-item>
-      </el-menu>
-    </el-aside>
-    <el-main>
-      <el-input v-model="current.title" placeholder="Title" size="large" />
-      <div class="flex gap-1 my-2 flex-wrap">
-        <el-button size="small" @click="format('**', '**')"><b>B</b></el-button>
-        <el-button size="small" @click="format('*', '*')"><i>I</i></el-button>
-        <el-button size="small" @click="format('# ')">H1</el-button>
-        <el-button size="small" @click="format('## ')">H2</el-button>
-        <el-button size="small" @click="format('### ')">H3</el-button>
-        <el-button size="small" @click="format('- ')">List</el-button>
-        <el-button size="small" @click="format('[](url)')">Link</el-button>
-        <el-button size="small" @click="format('`', '`')">Code</el-button>
-        <el-button size="small" @click="format('> ')">Quote</el-button>
+    </div>
+
+    <div class="flex-1 overflow-auto px-4 py-3">
+      <div v-if="memos.length" class="flex flex-col gap-3">
+        <div v-for="memo in memos" :key="memo.id" class="p-3 rounded-lg bg-[var(--el-fill-color-light)]">
+          <div class="flex items-start justify-between gap-2">
+            <div class="flex-1 text-sm whitespace-pre-wrap">{{ memo.content }}</div>
+            <el-button size="small" type="danger" link @click="deleteMemo(memo.id)">delete</el-button>
+          </div>
+          <div class="flex items-center gap-2 mt-2 text-xs text-[var(--el-text-color-secondary)]">
+            <span>{{ new Date(memo.createdAt).toLocaleString() }}</span>
+            <span v-for="tag in memo.tags" :key="tag" class="px-1.5 py-0.5 rounded bg-[var(--el-color-primary-light-9)] text-[var(--el-color-primary)]">#{{ tag }}</span>
+          </div>
+        </div>
       </div>
-      <div class="flex gap-2.5" style="height: calc(100vh - 160px)">
-        <el-input ref="editor" v-model="current.content" type="textarea" placeholder="Write markdown here..." class="flex-1 h-full" />
-        <div v-html="rendered" class="flex-1 border border-[var(--el-border-color)] p-2.5 overflow-auto"></div>
+      <div v-else class="flex items-center justify-center h-full text-sm text-[var(--el-text-color-secondary)]">
+        no memos yet
       </div>
-    </el-main>
-  </el-container>
+    </div>
+  </div>
 </template>
