71ab68d1af36238d530537f7f659ac6b92b16b95
Xun Lee <75558996+IlIllIIlIl@users.noreply.github.com>
2022-12-11 10:22:43 +0800

    refactor: use json locale files

--- apps/web/i18n.config.ts
+++ apps/web/i18n.config.ts
@@ -1,22 +1,11 @@
+import en from './locales/en.json'
+import zhHansCN from './locales/zh-Hans-CN.json'
+
 export default defineI18nConfig(() => ({
   legacy: false,
   locale: 'en',
   messages: {
-    en: {
-      'app.title': 'Rikki',
-      'input.placeholder': "what's on your mind...",
-      'input.send': 'send',
-      'settings.title': 'sync settings',
-      'settings.apiBase': 'api base url',
-      'settings.password': 'password',
-      'settings.cancel': 'cancel',
-      'settings.save': 'save',
-      'settings.loginFailed': 'login failed',
-      'settings.syncFailed': 'sync failed',
-      'action.sync': 'sync',
-      'action.delete': 'delete',
-      'action.settings': 'settings',
-      'empty': 'no notes yet',
-    },
+    en,
+    'zh-Hans-CN': zhHansCN,
   },
 }))
