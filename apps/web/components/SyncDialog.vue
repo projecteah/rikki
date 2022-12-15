7232544bef5f4fcab35a2c2ebcd5678b32fb3166
Xun Lee <75558996+IlIllIIlIl@users.noreply.github.com>
2022-12-15 16:28:43 +0800

    feat: language switcher in settings

--- apps/web/components/SyncDialog.vue
+++ apps/web/components/SyncDialog.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { SyncConfig } from '../../composables/useSync'
-const { t } = useI18n()
+const { t, locale, availableLocales } = useI18n()
 
 defineProps<{
   visible: boolean
@@ -25,11 +25,28 @@ watch(() => props.visible, (val) => {
 const onSave = () => {
   emit('save', apiBase.value, password.value)
 }
+
+const localeLabels: Record<string, string> = {
+  en: 'English',
+  'zh-Hans-CN': '简体中文',
+  'zh-Hant-TW': '正體中文',
+  lzh: '文言',
+}
 </script>
 
 <template>
   <el-dialog :model-value="visible" @update:model-value="emit('update:visible', $event)" :title="t('settings.title')" width="400px">
     <el-form label-position="top">
+      <el-form-item :label="t('settings.language')">
+        <el-select v-model="locale" class="w-full">
+          <el-option
+            v-for="loc in availableLocales"
+            :key="loc"
+            :label="localeLabels[loc] || loc"
+            :value="loc"
+          />
+        </el-select>
+      </el-form-item>
       <el-form-item :label="t('settings.apiBase')">
         <el-input v-model="apiBase" placeholder="https://your-api.vercel.app" />
       </el-form-item>
